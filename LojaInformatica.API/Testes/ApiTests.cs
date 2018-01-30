6002b8d77a9288bcb85baef9af61d77c1f8368aa
Thomaz Moura <padilha.thomaz@gmail.com>
2018-01-29 22:22:22 -0200

    Migrar testes genéricos de API para o ApiTests

--- LojaInformatica.API/Testes/ApiTests.cs
+++ LojaInformatica.API/Testes/ApiTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using LojaInformatica.API.Controllers;
 using LojaInformatica.API.Entidades;
@@ -11,6 +12,8 @@ namespace LojaInformatica.API.Testes
     public abstract class ApiTests<TEntidade> where TEntidade : Entidade
     {
         protected abstract IEntidadeApi<TEntidade> ObterApiController();
+        protected abstract IEnumerable<TEntidade> ObterExemploEntidades();
+        protected abstract void PersistirEntidades(IEnumerable<TEntidade> entidades);
 
         protected readonly AmbienteDeTeste _ambienteDeTeste;
         protected readonly IEntidadeApi<TEntidade> _controller;
@@ -23,11 +26,45 @@ namespace LojaInformatica.API.Testes
         [Fact]
         public void Api_Get_Deve_retornar_uma_lista_vazia_quando_não_houver_dados_no_banco()
         {
-
             var resultado = _controller.Get() as OkObjectResult;
             var clientes = resultado.Value as IEnumerable<TEntidade>;
 
             clientes.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Clientes_Get_Deve_retornar_a_lista_de_clientes_registrada_quando_houverem_clientes()
+        {
+            var entidades = ObterExemploEntidades();
+            PersistirEntidades(entidades);
+
+            var resultado = _controller.Get() as OkObjectResult;
+            var clientes = resultado.Value as IEnumerable<Cliente>;
+
+            clientes.Should().BeEquivalentTo(entidades);
+        }
+
+        [Fact]
+        public void Clientes_Get_Id_Deve_retornar_NotFound_quando_o_cliente_não_existir()
+        {
+            var idAusenteNoBanco = 404;
+
+            var resultado = _controller.Get(idAusenteNoBanco);
+
+            resultado.Should().BeOfType<NotFoundResult>();
+        }
+
+        [Fact]
+        public void Clientes_Get_Id_Deve_retornar_o_cliente_específico_se_existir()
+        {
+            var entidades = ObterExemploEntidades();
+            PersistirEntidades(entidades);
+            var clienteDeExemplo = entidades.First();
+
+            var resultado = _controller.Get(clienteDeExemplo.Id) as OkObjectResult;
+            var cliente = resultado.Value as Cliente;
+
+            cliente.ShouldBeEquivalentTo(clienteDeExemplo);
         }
     }
 }
--- LojaInformatica.API/Testes/ClienteApiTests.cs
+++ LojaInformatica.API/Testes/ClienteApiTests.cs
@@ -13,7 +13,6 @@ namespace LojaInformatica.API.Testes
     public class ClienteApiTests : ApiTests<Cliente>
     {
         private ClienteController _clienteController => _controller as ClienteController;
-
         public ClienteApiTests() { }
 
         protected override IEntidadeApi<Cliente> ObterApiController()
@@ -21,24 +20,38 @@ namespace LojaInformatica.API.Testes
             return new ClienteController(_ambienteDeTeste.Repositorio);
         }
 
-        [Fact]
-        public void Clientes_Get_Deve_retornar_a_lista_de_clientes_registrada_quando_houverem_clientes()
+        private readonly IEnumerable<Cliente> _clientes;
+        protected override IEnumerable<Cliente> ObterExemploEntidades()
         {
-            PersistirClientes(ExemplosDeClientes);
+            return new[]{
+                new Cliente{
+                    Id = 200,
+                    Nome = "Um Dois Três da Silva Quatro",
+                    Email = "1234@email.com",
+                    ChaveDeAcesso = new Guid("9690d1ba-f047-4974-a58b-9fe32e6a36d1")
+                },
+                new Cliente{
+                    Id = 201,
+                    Nome = "Fulano Cicrano Beltrano",
+                    Email = "johndoe@email.com",
+                    ChaveDeAcesso = new Guid("572db7a9-5a1d-4bed-a52e-af67ac033787")
+                }
+            };
+        }
 
-            var resultado = _controller.Get() as OkObjectResult;
-            var clientes = resultado.Value as IEnumerable<Cliente>;
-
-            clientes.Should().BeEquivalentTo(ExemplosDeClientes);
-            clientes.Count().Should().Be(2);
+        protected override void PersistirEntidades(IEnumerable<Cliente> clientes)
+        {
+            _ambienteDeTeste.Contexto.Clientes.AddRange(clientes);
+            _ambienteDeTeste.Contexto.SaveChanges();
         }
 
         [Fact]
         public void Clientes_Get_Com_nome_Deve_retornar_a_lista_de_clientes_filtrada_por_nome()
         {
             var filtroDeNome = "beltrano";
-            PersistirClientes(ExemplosDeClientes);
-            var clientesFiltrados = ExemplosDeClientes.AsQueryable()
+            var entidades = ObterExemploEntidades();
+            PersistirEntidades(entidades);
+            var clientesFiltrados = entidades.AsQueryable()
                 .PorNome(filtroDeNome);
 
             var resultado = _clienteController.Get(filtroDeNome) as OkObjectResult;
@@ -46,48 +59,5 @@ namespace LojaInformatica.API.Testes
 
             clientes.Should().BeEquivalentTo(clientesFiltrados);
         }
-
-        [Fact]
-        public void Clientes_Get_Id_Deve_retornar_NotFound_quando_o_cliente_não_existir()
-        {
-            var idAusenteNoBanco = 404;
-
-            var resultado = _controller.Get(idAusenteNoBanco);
-
-            resultado.Should().BeOfType<NotFoundResult>();
-        }
-
-        [Fact]
-        public void Clientes_Get_Id_Deve_retornar_o_cliente_específico_se_existir()
-        {
-            PersistirClientes(ExemplosDeClientes);
-            var clienteDeExemplo = ExemplosDeClientes.First();
-
-            var resultado = _controller.Get(clienteDeExemplo.Id) as OkObjectResult;
-            var cliente = resultado.Value as Cliente;
-
-            cliente.ShouldBeEquivalentTo(clienteDeExemplo);
-        }
-
-        public void PersistirClientes(IEnumerable<Cliente> clientes)
-        {
-            _ambienteDeTeste.Contexto.Clientes.AddRange(clientes);
-            _ambienteDeTeste.Contexto.SaveChanges();
-        }
-
-        public IEnumerable<Cliente> ExemplosDeClientes = new[]{
-            new Cliente{
-                Id = 200,
-                Nome = "Um Dois Três da Silva Quatro",
-                Email = "1234@email.com",
-                ChaveDeAcesso = new Guid("9690d1ba-f047-4974-a58b-9fe32e6a36d1")
-            },
-            new Cliente{
-                Id = 201,
-                Nome = "Fulano Cicrano Beltrano",
-                Email = "johndoe@email.com",
-                ChaveDeAcesso = new Guid("572db7a9-5a1d-4bed-a52e-af67ac033787")
-            }
-        };
     }
 }
